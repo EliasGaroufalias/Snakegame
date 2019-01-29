ed20aa406767ba5dd529d82e0071181b94589ce6
EliasGaroufalias <eliassailafourag@gmail.com>
2019-01-29 16:58:33 +0200

    First commit

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ImageLocation = "snekhead0.png";
+            this.pictureBox1.Location = new System.Drawing.Point(190, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(274, 143);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 90;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(657, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 26);
+            this.label1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.Font = new System.Drawing.Font("Constantia", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(280, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Play!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Constantia", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(280, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Paused";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(722, 724);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        List<PictureBox> pictureBoxList = new List<PictureBox>();
+        List<Point> locationList = new List<Point>();
+        List<int> sneknum = new List<int>();
+        PictureBox pictureBoxN = new PictureBox();
+        Point lastFoodLocation = new Point();
+        int dir = 0;
+        int length1 = 0;
+        bool appending = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void MoveSnake(List<PictureBox> pictureboxlist, List<int> sneknum, int direction)
+        {
+            length1++;
+            locationList.Clear();
+            for (int i = pictureboxlist.Count()-1; i > 0; i--)
+            {
+                pictureboxlist[i].Location = pictureboxlist[i - 1].Location;
+                sneknum[i] = sneknum[i - 1];
+                pictureboxlist[i].Image = (Image)(Properties.Resources.ResourceManager.GetObject("snek" + sneknum[i].ToString()));
+                pictureboxlist[i].Size = new Size(25, 25);
+                locationList.Add(pictureBoxList[i].Location);
+            }
+            if (pictureboxlist.Count() == length1 && appending == true)
+            {
+                PictureBox pictureBoxN = new PictureBox();
+                pictureBoxN.Image = (Image)(Properties.Resources.ResourceManager.GetObject("snek" + sneknum[sneknum.Count()-1].ToString()));
+                pictureBoxN.Location = pictureboxlist.Last().Location;
+                pictureBoxN.BackgroundImageLayout = ImageLayout.Stretch;
+                pictureboxlist.Add(pictureBoxN);
+                sneknum.Add(direction);
+                Controls.Add(pictureBoxN);
+                appending = false;
+            }
+            switch (direction % 4)
+            {
+                case 0:
+                    pictureBox1.Left += 24;
+                    break;
+                case 1:
+                    pictureBox1.Top -= 24;
+                    break;
+                case 2:
+                    pictureBox1.Left -= 24;
+                    break;
+                case 3:
+                    pictureBox1.Top += 24;
+                    break;
+            }
+            pictureBox1.Image = (Image)(Properties.Resources.ResourceManager.GetObject("snekhead" + direction.ToString()));
+            sneknum[0] = direction;
+            if (locationList.Contains(pictureBox1.Location) || (pictureBox1.Top < 0) || (pictureBox1.Top >= 720) || (pictureBox1.Left < 0) || (pictureBox1.Left >= 720))
+            {
+                Die();
+            }
+            else if (pictureBox1.Location == pictureBox2.Location)
+            {
+                pictureBox2.Location = new Point(r.Next(0, 29) * 24, r.Next(0, 29) * 24);
+                while (locationList.Contains(pictureBox2.Location))
+                {
+                    pictureBox2.Location = new Point(r.Next(0, 29) * 24, r.Next(0, 29) * 24);
+                }
+                lastFoodLocation = pictureBox2.Location;
+                length1 = 1;
+                appending = true;
+                label1.Text = (pictureboxlist.Count() - 1).ToString();
+            }
+        }
+
+        private void Die()
+        {
+            timer1.Enabled = false;
+            button1.Show();
+            button2.Hide();
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            foreach (PictureBox p in pictureBoxList)
+            {
+                p.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Die();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar == (char)Keys.D) || (e.KeyChar == (char)100)) && (dir != 2 ))
+            {
+                dir = 0;
+            }
+            else if (((e.KeyChar == (char)Keys.W) || (e.KeyChar == (char)119)) && (dir != 3))
+            {
+                dir = 1;
+            }
+            else if (((e.KeyChar == (char)Keys.A) || (e.KeyChar == (char)97)) && (dir != 0))
+            {
+                dir = 2;
+            }
+            else if (((e.KeyChar == (char)Keys.S) || (e.KeyChar == (char)115)) && (dir != 1))
+            {
+                dir = 3;
+            }
+            else if (e.KeyChar == (char)Keys.Escape)
+            {
+                timer1.Enabled = timer1.Enabled ^ true;
+                button2.Visible = button2.Visible ^ true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveSnake(pictureBoxList, sneknum, dir);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            label1.Text = "0";
+            button2.Hide();
+            pictureBox1.Show();
+            pictureBox2.Show();
+            pictureBoxList.Clear();
+            pictureBoxList.Add(pictureBox1);
+            sneknum.Add(dir);
+            PictureBox pictureBoxN = new PictureBox();
+            pictureBoxN.ImageLocation = "snek0.png";
+            pictureBoxN.Location = pictureBoxList.Last().Location;
+            pictureBoxN.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBoxList.Add(pictureBoxN);
+            sneknum.Add(dir);
+            Controls.Add(pictureBoxN);
+            pictureBox1.Location = new Point(r.Next(10, 20) * 24, r.Next(10, 20) * 24);
+            pictureBox2.Location = new Point(r.Next(0, 29) * 24, r.Next(0, 29) * 24);
+            button1.Hide();
+            timer1.Enabled = true;
+        }
+
+    }
+}
